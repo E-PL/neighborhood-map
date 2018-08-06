7161fe5daabe72fa79a7166d87588349cfebdaed
e-pl <507904+E-PL@users.noreply.github.com>
2018-08-06 23:49:54 +0200

    Fix: api errror handling, styles, readme

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-function gm_authFailure() {
-    document.alert('no api');
-    console.log('errror')
-  
-  }
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
